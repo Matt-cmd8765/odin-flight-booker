00dd155bfb57e9bb4c7663144335079cfac20a05
Matt Marini <mattmarini8765@gmail.com>
2024-02-25 12:35:46 +1100

    Move form outside table. Does not work in table

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,5 +1,5 @@
 class Passenger < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true, uniqueness: true 
+  validates :name, presence: true, on: :create
+  validates :email, presence: true, uniqueness: true, on: :create
   belongs_to :booking
 end
