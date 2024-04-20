66f86cead2d4d0bdd3973d8ca31a4548573ce66b
Matt Marini <mattmarini8765@gmail.com>
2024-04-20 20:28:11 +1000

    Add mailer

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -11,6 +11,7 @@ class BookingsController < ApplicationController
   
     respond_to do |format|
       if @booking.save
+        PassengerMailer.with(booking: @booking).welcome_email.deliver_later
         format.html { redirect_to booking_url(@booking), notice: 'Booking was successfully created.' }
         format.json { render :show, status: :created, location: @booking }
       else
--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,10 +1,14 @@
 class PassengerMailer < ApplicationMailer
   # This and welcome email from https://guides.rubyonrails.org/action_mailer_basics.html section 2.1.2
   default from: 'notifications@example.com'
-
+  
+  # TODO: update user to passenger
   def welcome_email
-    @user = params[:user]
+    @booking = params[:booking]
     @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+    @booking.passengers.each do |passenger|
+      @passenger = passenger
+      mail(to: passenger.email, subject: 'Booking Confirmed')
+    end
   end
 end
